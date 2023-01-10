f5e14d54dac1ce6c7654b2565e635670bc5e85e6
Bluebender <olliviev@gmail.com>
2023-01-10 16:17:45 +0000

    Sauvegarde de 17h

--- exercice5-canevas/index.js
+++ exercice5-canevas/index.js
@@ -85,17 +85,157 @@ function exercice4()
 
 function exercice5()
 {
-// Dessiner un cercle bleu d'un rayon de 10px, au centre de votre canvas.
-// Toutes les 0.1 secondes,le rayon du cercle va augmenter de 10px.
-// Quand le rayon du cercle est égal à la moitié de la dimension la plus petite du canvas, arrêtez le grossissement.
-
-
-
+    let circle = {
+        color: "blue",
+        radius:10,
+        x:220,
+        y:135
+    };
+    let canvasDom = document.querySelector('#ex5');
+    let ctx = canvasDom.getContext('2d');
+    ctx.fillStyle = circle.color;
+    
+    let radius_max
+    if (canvas.height<canvas.width){
+        radius_max = canvas.height/2-5;
+    }
+    else{
+        radius_max = canvas.width/2-5;
+    }
+    // console.log(canvas.height)
+    // console.log(canvas.width)
+    // console.log(radius_max)
+    
+    let radius = 10;
+    let intervalId = setInterval(function(){
+        if (radius < radius_max){
+            ctx.clearRect(0, 0, canvasDom.width, canvasDom.height);
+            ctx.beginPath(); 
+            ctx.arc(circle.x, circle.y, radius, 0, 2 * Math.PI);
+            ctx.fill(); 
+            // console.log(x);
+            radius=radius+10;
+        }
+        else{
+            clearInterval(intervalId);
+        }
+    }, 100);
 }
 
 function exercice6()
 {
+// Vous avez dans les fichiers des exercices, un fichier pacman.png avec un pacman bouche ouverte d'environ 100px de rayon.
+// Faites en sorte de lui faire ouvrir et fermer la bouche toutes les secondes, et faites en sorte que le fond de votre canvas soit gris foncé.
 
+
+    let yellow = "#FFD65B";
+
+    // Déclaration du canvevas        
+    let canvasDom = document.querySelector('#ex6');
+    let ctx = canvasDom.getContext('2d');
+
+    // Couleur de fond
+    // On dit au contexte que la couleur de remplissage est gris 
+    ctx.fillStyle = '#DDDDDD'; 
+    // On rempli le Canvas de gris en fond 
+    ctx.fillRect(0, 0, canvasDom.width, canvasDom.height);
+
+    // Ajout de l'image    
+    // On créer un nouvel Objet du DOM avec JS.
+    let myImg = new Image();
+    // On définie la source de cette image
+    myImg.src = 'pacman.png';
+    // Quand l'image est chargée par le navigateur on la place dans le Canvas
+    myImg.onload = function () {
+        //On place l'image
+        ctx.drawImage(myImg,100,50);
+    };
+    // ctx.drawImage(image, dx, dy, dLargeur, dHauteur); 
+
+    let circle = {
+    color: yellow,
+    radius:100,
+    x:200,
+    y:150
+    };
+
+    let swich=0;
+    let i = 0;
+    
+    let intervalId = setInterval(function(){
+        if (i<20){
+
+            if (swich===0){
+                
+                ctx.clearRect(0, 0, canvasDom.width, canvasDom.height);
+    
+                // Couleur de fond
+                // On dit au contexte que la couleur de remplissage est gris 
+                ctx.fillStyle = '#DDDDDD'; 
+                // On rempli le Canvas de gris en fond 
+                ctx.fillRect(0, 0, canvasDom.width, canvasDom.height);
+        
+                ctx.fillStyle = circle.color;
+                ctx.beginPath(); 
+                ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+                ctx.fill();
+                
+                swich=1;
+            }
+            else{
+                
+                ctx.clearRect(0, 0, canvasDom.width, canvasDom.height);
+    
+                // Couleur de fond
+                // On dit au contexte que la couleur de remplissage est gris 
+                ctx.fillStyle = '#DDDDDD'; 
+                // On rempli le Canvas de gris en fond 
+                ctx.fillRect(0, 0, canvasDom.width, canvasDom.height);
+    
+                // Ajout de l'image    
+                // On créer un nouvel Objet du DOM avec JS.
+                let myImg = new Image();
+                // On définie la source de cette image
+                myImg.src = 'pacman.png';
+                // Quand l'image est chargée par le navigateur on la place dans le Canvas
+                myImg.onload = function () {
+                    //On place l'image
+                    ctx.drawImage(myImg,100,50);
+                };
+                // ctx.drawImage(image, dx, dy, dLargeur, dHauteur); 
+    
+                swich=0;            
+            }
+            i=i+1
+            console.log(i)
+        }
+        else{
+            clearInterval(intervalId);
+        }
+
+
+    }, 0.2*1000);
+
+        
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
 function initCanvasSize()
