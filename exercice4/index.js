d083876e79fffdeeaea365f00cc2d94f024fd74c
Bluebender <olliviev@gmail.com>
2023-01-10 11:01:02 +0000

    Sauvegarde de 12h

--- exercice4/index.js
+++ exercice4/index.js
@@ -3,10 +3,13 @@ let numbers = [32, 43, 56, 24, 78, 42];
 // Toutes les secondes, affichez un nouveau nombre du tableau. Lorsque vous arrivez à la fin du tableau : stoppez le processus.
 
 window.addEventListener("DOMContentLoaded", function(){
-
-    for (number of numbers){
-        console.log(number);
-        let timeoutId = setTimeout(function(){
+        
+        let i = 0;
+        let intervalId = setInterval(function(){
+        if (i < numbers.length){
+            console.log(numbers[i]);
+            i=i+1;
+        }
         }, 1000);
-    }
+
 });
